8063de2b6fa1af3b85a12445ed838c5d1fcb48ae
Carlos-Liu <zliu02@beckman.com>
2016-01-19 14:50:59 +0800

    commit the missed file
    the file is missed to commit

--- src/FileSizeCounter/MicroMvvm/PropertySupport.cs
+++ src/FileSizeCounter/MicroMvvm/PropertySupport.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Linq.Expressions;
 using System.Reflection;
-using FileSizeCounter.Properties;
+using Res;
 
 namespace FileSizeCounter.MicroMvvm
 {
   /// <summary>
-  /// Facility to check if there is the property in an instance.
+  ///   Facility to check if there is the property in an instance.
   /// </summary>
   public static class PropertySupport
   {
     /// <summary>
-    /// Debug facility to help find the issues that the instance does not contain a property.
+    ///   Debug facility to help find the issues that the instance does not contain a property.
     /// </summary>
     /// <typeparam name="T">Property type.</typeparam>
     /// <param name="propertyExpresssion">property expression.</param>
     /// <returns>Property name.</returns>
-    public static String ExtractPropertyName<T>(Expression<Func<T>> propertyExpresssion)
+    public static string ExtractPropertyName<T>(Expression<Func<T>> propertyExpresssion)
     {
       if (propertyExpresssion == null)
       {
@@ -26,13 +26,15 @@ namespace FileSizeCounter.MicroMvvm
       var memberExpression = propertyExpresssion.Body as MemberExpression;
       if (memberExpression == null)
       {
-        throw new ArgumentException(Resources.PropertySupport_Error_ExpressionIsNotMemberExpression, "propertyExpresssion");
+        throw new ArgumentException(Resources.PropertySupport_Error_ExpressionIsNotMemberExpression,
+          "propertyExpresssion");
       }
 
       var property = memberExpression.Member as PropertyInfo;
       if (property == null)
       {
-        throw new ArgumentException(Resources.PropertySupport_Error_MemberExpressionNotAccessProperty, "propertyExpresssion");
+        throw new ArgumentException(Resources.PropertySupport_Error_MemberExpressionNotAccessProperty,
+          "propertyExpresssion");
       }
 
       var getMethod = property.GetGetMethod(true);
